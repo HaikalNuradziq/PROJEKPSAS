c672e27bcc4f53520bc82a188f4278c5a7d2427f
Muhammad Raja <rajasudibya@gmail.com>
2026-06-10 10:51:00 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "tradelab";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+?>
 
